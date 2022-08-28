03807ee6538ab9aafa9e0c7a1dce0c297fc79dae
AndrewFaugno <andrewfaugno825@gmail.com>
2022-08-27 23:23:38 -0400

    added auth.logged in to nav with icons

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -1,15 +1,24 @@
+import Auth from '../../utils/auth.js'
+
 const Nav = () => {
+    const loggedIn = Auth.loggedIn();
+
     return (
         <div className="bg-dark">
             <div className="d-flex justify-content-around">
                 <div className="m-auto">
                     <h1>
-                        <a href="/" className="">Shop Name</a>
+                        <a href="/" className="">Import Export</a>
                     </h1>
                 </div>
                 <div className="m-auto">
-                    <a href="/login" className="">Login</a>
-                    <a href="/signup">Signup</a>
+                    {loggedIn ? (
+                        <span class="material-symbols-outlined">account_circle</span>    
+                    ) : (
+                    <a href="/login" className="mr-3">
+                        <span class="material-symbols-outlined">login</span>
+                    </a>
+                    )}
                 </div>
             </div>
         </div>
